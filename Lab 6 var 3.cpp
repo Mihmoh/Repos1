30ca6faf7d5246ebc1318387330ab19b97483fb4
Mihmoh <71183341+Mihmoh@users.noreply.github.com>
2021-12-02 18:58:13 +0300

    Update Lab 6 var 3.cpp

--- Lab 6 var 3.cpp
+++ Lab 6 var 3.cpp
@@ -37,6 +37,11 @@ public:
 	}
 	virtual int Counting() = 0;
 	virtual void printInfo() = 0;
+	virtual void init() = 0;
+	friend void sortName(worker**&, const int&);
+	friend void sortWorkDays(worker**&, const int&);
+	friend void sortWorkHours(worker**&, const int&);
+	friend void sortTotalWork(worker**&, const int&);
 };
 
 class engineer : public worker
@@ -48,31 +53,51 @@ protected:
 public:
 	engineer()
 	{
-		name = "Vasiliy";
+		name = "Ivan";
 		workDays = 5;
 		workHours = 8;
 		totalWork = 40;
 	}
-	engineer(string Name, int WD, int WH):worker(Name)
+	engineer(string name, int WD, int WH, int TW):worker(name), workDays (WD), workHours(WH), totalWork(0)
 	{
-		name = Name;
 		workDays = WD;
 		workHours = WH;
+		totalWork = 0;
 	}
 	~engineer() 
 	{
 		cout << "Destructor of engineer..." << endl;
 	}
+	void init() override
+	{
+		cout << "Enter name..." << endl;
+		cin >> name;
+		cout << "Enter work days..." << endl;
+		workDays = checkInt();
+		cout << "Enter work hours..." << endl;
+		workHours = checkInt();
+	}
 	int Counting() override
 	{
-		cout << "Counting total work of engineer..." << endl;
+		cout << "Total work = work days * work hours..." << endl;
 		totalWork = workHours * workDays;
 		return totalWork;
 	}
 	void printInfo() override
 	{
 		cout << "Name: " << name << "\n work days: " << workDays << "\n work hours: " << workHours << "\n total work: " << totalWork << endl;
+		cout << "___________________________________" << endl;
 	}
+	engineer& operator () (string Name, int WD, int WH)
+	{
+		name = Name;
+		workDays = WD;
+		workHours = WH;
+		return*this;
+	}
+	friend void sortWorkDays(worker**& , const int& );
+	friend void sortWorkHours(worker**&, const int&);
+	friend void sortTotalWork(worker**&, const int&);
 };
 
 class salary: public engineer
@@ -83,34 +108,45 @@ protected:
 public:
 	salary()
 	{
-		name = "Vasiliy";
-		workDays = 5;
-		workHours = 8;
-		totalWork = 40;
+		name = "Boris";
+		workHours = 6;
+		workDays = 7;
+		totalWork = 42;
 		moneyPerHour = 10;
-		totalSalary = 400;
+		totalSalary = 420;
 	}
-	salary(string Name, int WD, int WH, int MPH)
+	salary(string name, int workDays, int workHours, int totalWork, int MPH): engineer(name, workDays, workHours, totalWork), moneyPerHour(MPH)
 	{
-		name = Name;
-		workDays = WD;
-		workHours = WH;
 		moneyPerHour = MPH;
 	}
 	~salary()
 	{
 		cout << "Destructor of salary..." << endl;
 	}
+	void init() override
+	{
+		cout << "Enter name..." << endl;
+		cin >> name;
+		cout << "Enter work days..." << endl;
+		workDays = checkInt();
+		cout << "Enter work hours..." << endl;
+		workHours = checkInt();
+		cout << "Enter money per hour..." << endl;
+		moneyPerHour = checkInt();
+	}
 	int Counting() override
 	{
-		cout << "Counting total salary of engineer..." << endl;
+		totalWork = workDays * workHours;
+		cout << "Total salary = total work * money per hour..." << endl;
 		totalSalary = totalWork * moneyPerHour;
 		return totalSalary;
 	}
 	void printInfo() override
 	{
 		cout << "Name: " << name << "\n work days: " << workDays << "\n work hours: " << workHours << "\n total work: " << totalWork<<"\n money per hour: "<< moneyPerHour<<"\n total salary: "<< totalSalary << endl;
+		cout << "___________________________________" << endl;
 	}
+	friend void sortMPH(worker**&, const int&);
 };
 
 class medic : public worker
@@ -121,42 +157,443 @@ protected:
 public:
 	medic()
 	{
-		name = "Arcadiy";
+		name = "Alexander";
 		quantity_Of_Patients = 30;
 		time_Of_work = 150;
 	}
-	medic(string Name, int QOP)
+	medic(string name, int QOP):worker(name), quantity_Of_Patients(QOP)
 	{
-		name = Name;
 		quantity_Of_Patients = QOP;
 	}
 	~medic()
 	{
 		cout << "Destructor of medic..." << endl;
 	}
+	void init() override
+	{
+		cout << "Enter name..." << endl;
+		cin >> name;
+		cout << "Enter quantity of patients..." << endl;
+		quantity_Of_Patients = checkInt();
+	}
 	int Counting() override
 	{
-		cout << "Counting time of work of medic..." << endl;
+		cout << "Time of work = quantity of patients * 5..." << endl;
 		time_Of_work = quantity_Of_Patients * 5;
 		return time_Of_work;
 	}
 	void printInfo() override
 	{
 		cout << "Name: " << name << "\n quantity of patients: " << quantity_Of_Patients << "\n time of work: " << time_Of_work  << endl;
+		cout << "___________________________________" << endl;
 	}
+	friend void sortQOP(worker**& , const int& );
+	friend void sortTOW(worker**&, const int&);
 };
+
+void printMenu()
+{
+	cout << "1 - add element " << endl;
+	cout << "2 - print all " << endl;
+	cout << "3 - sort by name " << endl;
+	cout << "4 - sort by work days " << endl;
+	cout << "5 - sort by work hours " << endl;
+	cout << "6 - sort by total work " << endl;
+	cout << "7 - sort by money per hour " << endl;
+	cout << "8 - sort by quantity of patients " << endl;
+	cout << "9 - sort by time of work " << endl;
+	cout << "0 - exit " << endl;
+}
+
+int Strcmp(string string1, string string2)
+{
+	int i = 0;
+	int flag = 0;
+	while (flag == 0)
+	{
+		if (string1[i] > string2[i])
+		{
+			flag = 1;
+		}
+		else if (string1[i] < string2[i])
+		{
+			flag = -1;
+		}
+
+		if (string1[i] == '\0')
+		{
+			break;
+		}
+
+		i++;
+	}
+	return flag;
+}
+
+void sortName(worker**& mas, const int& size)
+{
+	cout << "1 - by uprising, 2 - by decreasing: " << endl;
+	int  option;
+	option = checkInt();
+	switch (option)
+	{
+	case 1:
+		for (int i = 0; i < size; ++i)
+			for (int j = i + 1; j < size; ++j)
+				if (Strcmp(mas[i]->name, mas[j]->name) > 0)
+					swap(mas[i], mas[j]);
+		break;
+	case 2:
+		for (int i = 0; i < size; ++i)
+			for (int j = i + 1; j < size; ++j)
+				if (Strcmp(mas[i]->name, mas[j]->name) < 0)
+					swap(mas[i], mas[j]);
+		break;
+	default:
+		break;
+	}	
+}
+
+void sortWorkDays(worker**& mas, const int& size)
+{
+	engineer *A, * B;
+	cout << "1 - by uprising, 2 - by decreasing: " << endl;
+	int  option;
+	option = checkInt();
+	switch (option)
+	{
+	case 1:
+		for (int i = 0; i < size; ++i)
+			for (int j = i + 1; j < size; ++j)
+			{
+				A = (engineer *)mas[i];
+				B = (engineer *)mas[j];
+				if (A->workDays > B->workDays)
+					swap(mas[i], mas[j]);
+			}
+		break;
+	case 2:
+		for (int i = 0; i < size; ++i)
+			for (int j = i + 1; j < size; ++j)
+			{
+				A = (engineer*)mas[i];
+				B = (engineer*)mas[j];
+				if (A->workDays < B->workDays)
+					swap(mas[i], mas[j]);
+			}
+		break;
+	default:
+		break;
+	}
+
+}
+
+void sortWorkHours(worker**& mas, const int& size)
+{
+	engineer* A, * B;
+	cout << "1 - by uprising, 2 - by decreasing: " << endl;
+	int  option;
+	option = checkInt();
+	switch (option)
+	{
+	case 1:
+		for (int i = 0; i < size; ++i)
+			for (int j = i + 1; j < size; ++j)
+			{
+				A = (engineer*)mas[i];
+				B = (engineer*)mas[j];
+				if (A->workHours > B->workHours)
+					swap(mas[i], mas[j]);
+			}
+		break;
+	case 2:
+		for (int i = 0; i < size; ++i)
+			for (int j = i + 1; j < size; ++j)
+			{
+				A = (engineer*)mas[i];
+				B = (engineer*)mas[j];
+				if (A->workHours < B->workHours)
+					swap(mas[i], mas[j]);
+			}
+		break;
+	default:
+		break;
+	}
+
+}
+
+void sortTotalWork(worker**& mas, const int& size)
+{
+	engineer* A, * B;
+	cout << "1 - by uprising, 2 - by decreasing: " << endl;
+	int  option;
+	option = checkInt();
+	switch (option)
+	{
+	case 1:
+		for (int i = 0; i < size; ++i)
+			for (int j = i + 1; j < size; ++j)
+			{
+				A = (engineer*)mas[i];
+				B = (engineer*)mas[j];
+				if (A->totalWork > B->totalWork)
+					swap(mas[i], mas[j]);
+			}
+		break;
+	case 2:
+		for (int i = 0; i < size; ++i)
+			for (int j = i + 1; j < size; ++j)
+			{
+				A = (engineer*)mas[i];
+				B = (engineer*)mas[j];
+				if (A->totalWork < B->totalWork)
+					swap(mas[i], mas[j]);
+			}
+		break;
+	default:
+		break;
+	}
+
+}
+
+void sortMPH(worker**& mas, const int& size)
+{
+	salary* A, * B;
+	cout << "1 - by uprising, 2 - by decreasing: " << endl;
+	int  option;
+	option = checkInt();
+	switch (option)
+	{
+	case 1:
+		for (int i = 0; i < size; ++i)
+			for (int j = i + 1; j < size; ++j)
+			{
+				A = (salary*)mas[i];
+				B = (salary*)mas[j];
+				if (A->moneyPerHour > B->moneyPerHour)
+					swap(mas[i], mas[j]);
+			}
+		break;
+	case 2:
+		for (int i = 0; i < size; ++i)
+			for (int j = i + 1; j < size; ++j)
+			{
+				A = (salary*)mas[i];
+				B = (salary*)mas[j];
+				if (A->moneyPerHour < B->moneyPerHour)
+					swap(mas[i], mas[j]);
+			}
+		break;
+	default:
+		break;
+	}
+
+}
+
+void sortQOP(worker**& mas, const int& size)
+{
+	medic* A, * B;
+	cout << "1 - by uprising, 2 - by decreasing: " << endl;
+	int  option;
+	option = checkInt();
+	switch (option)
+	{
+	case 1:
+		for (int i = 0; i < size; ++i)
+			for (int j = i + 1; j < size; ++j)
+			{
+				A = (medic*)mas[i];
+				B = (medic*)mas[j];
+				if (A->quantity_Of_Patients > B->quantity_Of_Patients)
+					swap(mas[i], mas[j]);
+			}
+		break;
+	case 2:
+		for (int i = 0; i < size; ++i)
+			for (int j = i + 1; j < size; ++j)
+			{
+				A = (medic*)mas[i];
+				B = (medic*)mas[j];
+				if (A->quantity_Of_Patients < B->quantity_Of_Patients)
+					swap(mas[i], mas[j]);
+			}
+		break;
+	default:
+		break;
+	}
+
+}
+
+void sortTOW(worker**& mas, const int& size)
+{
+	medic* A, * B;
+	cout << "1 - by uprising, 2 - by decreasing: " << endl;
+	int  option;
+	option = checkInt();
+	switch (option)
+	{
+	case 1:
+		for (int i = 0; i < size; ++i)
+			for (int j = i + 1; j < size; ++j)
+			{
+				A = (medic*)mas[i];
+				B = (medic*)mas[j];
+				if (A->time_Of_work > B->time_Of_work)
+					swap(mas[i], mas[j]);
+			}
+		break;
+	case 2:
+		for (int i = 0; i < size; ++i)
+			for (int j = i + 1; j < size; ++j)
+			{
+				A = (medic*)mas[i];
+				B = (medic*)mas[j];
+				if (A->time_Of_work < B->time_Of_work)
+					swap(mas[i], mas[j]);
+			}
+		break;
+	default:
+		break;
+	}
+
+}
 
 int main()
 {
+	engineer a("0", 0, 0, 0);
+	salary b("0", 0, 0, 0, 0);
+	medic c("0", 0);
+	int size = 3;
 	worker **proletariy;
-	proletariy = new worker*[3];
-	proletariy[0] = new engineer;
-	proletariy[1] = new salary;
-	proletariy[2] = new medic;
-	for (int i = 0; i < 3; i++)
+	proletariy = new worker * [size];
+	int option = 100;
+	cout << "Choose type of initialization: 1 - default, 2 - manualy" << endl;
+	option = checkInt();
+	switch (option)
 	{
-		proletariy[i]->Counting();
-		proletariy[i]->printInfo();
+	case 1:
+		proletariy[0] = new engineer;
+		proletariy[1] = new salary;
+		proletariy[2] = new medic;
+		size = 3;
+		break;
+	case 2:
+		size = 0;
+		proletariy = new worker * [size];
+		break;
+	}	
+	while (option != 0)
+	{
+		printMenu();
+		option = checkInt();
+		switch (option)
+		{
+		case 1:			
+			while (option != 0)
+			{
+				system("cls");
+				cout << "Choose type of object: 1 - engineer, 2 - salary, 3 - medic, 0 - stop " << endl;
+				option = checkInt();
+				switch (option)
+				{
+				case 1:
+					proletariy[size] = new engineer;
+					proletariy[size]->init();
+					proletariy[size]->Counting();
+					size++;					
+					break;
+				case 2:
+					proletariy[size] = new salary;
+					proletariy[size]->init();					
+					proletariy[size]->Counting();
+					size++;
+					break;
+				case 3:
+					proletariy[size] = new medic;
+					proletariy[size]->init();
+					proletariy[size]->Counting();
+					size++;
+					break;
+				default:
+					break;
+				}
+			}		
+			option = 100;
+			break;
+		case 2:
+			system("cls");
+			for (int i = 0; i < size; i++)
+			{
+				proletariy[i]->printInfo();
+			}
+			break;
+		case 3:
+			system("cls");
+			sortName(proletariy, size);
+			for (int i = 0; i < size; i++)
+			{
+				proletariy[i]->printInfo();
+			}
+			break;
+		case 4:
+			system("cls");
+			sortWorkDays(proletariy, size);
+			for (int i = 0; i < size; i++)
+			{
+				if (typeid(*proletariy[i]).name() == typeid(a).name() || typeid(*proletariy[i]).name() == typeid(b).name())
+					proletariy[i]->printInfo();
+			}
+			break;
+		case 5:
+			system("cls");
+			sortWorkHours(proletariy, size);
+			for (int i = 0; i < size; i++)
+			{
+				if (typeid(*proletariy[i]).name() == typeid(a).name() || typeid(*proletariy[i]).name() == typeid(b).name())
+					proletariy[i]->printInfo();
+			}
+			break;
+		case 6:
+			system("cls");
+			sortTotalWork(proletariy, size);
+			for (int i = 0; i < size; i++)
+			{
+				if (typeid(*proletariy[i]).name() == typeid(a).name()|| typeid(*proletariy[i]).name() == typeid(b).name())
+					proletariy[i]->printInfo();
+			}
+			break;
+		case 7:
+			system("cls");
+			sortMPH(proletariy, size);			
+			for (int i = 0; i < size; i++)
+			{
+				if(typeid(*proletariy[i]).name()== typeid(b).name())
+					proletariy[i]->printInfo();				
+			}
+			break;
+		case 8:
+			system("cls");
+			sortQOP(proletariy, size);
+			for (int i = 0; i < size; i++)
+			{
+				if (typeid(*proletariy[i]).name() == typeid(c).name())
+					proletariy[i]->printInfo();
+			}
+			break;
+		case 9:
+			system("cls");
+			sortTOW(proletariy, size);
+			for (int i = 0; i < size; i++)
+			{
+				if (typeid(*proletariy[i]).name() == typeid(c).name())
+					proletariy[i]->printInfo();
+			}
+			break;
+		case 0:
+			for (int i = 0; i < size; i++)
+				delete[] proletariy[i];
+			return 0;
+		}
 	}
-	delete[] proletariy;
 }
+//меню, удаление дубликатов, поиск по диапазону, сортировка по любому полю
